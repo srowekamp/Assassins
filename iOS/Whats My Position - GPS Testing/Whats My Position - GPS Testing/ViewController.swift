e84b13ef42381f8977a41543f8a6863429e4da16
Scott Rowekamp <rowekamp@iastate.edu>
2016-09-21 18:48:50 -0500

    GPS Testing Working Product

--- iOS/Whats My Position - GPS Testing/Whats My Position - GPS Testing/ViewController.swift
+++ iOS/Whats My Position - GPS Testing/Whats My Position - GPS Testing/ViewController.swift
@@ -7,17 +7,55 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapData: MKMapView!
+
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBAction func getLocation(_ sender: AnyObject) {
+        print(mapData.userLocation.location)
+        locationLabel.text = "Your latitude is: \((locMan.location?.coordinate.latitude)!)"
+    }
+    
+    let locMan = CLLocationManager();
+    
     override func viewDidLoad() {
+        
+        
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.locMan.delegate = self
+        self.locMan.desiredAccuracy = kCLLocationAccuracyBest
+        self.locMan.requestWhenInUseAuthorization()
+        self.locMan.startUpdatingLocation()
+        
+        
+        mapData.delegate = self
+        mapData.userTrackingMode = MKUserTrackingMode(rawValue: 1)!
+        mapData.showsUserLocation = true
+        locationLabel.text = "Location: Unknown"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Location Delegate Methods
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        let center = CLLocationCoordinate2DMake((location?.coordinate.latitude)!, (location?.coordinate.longitude)!)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpanMake(1, 1))
+        mapData.setRegion(region, animated: true)
+        locMan.stopUpdatingLocation()
+        
     }
 
 
