bb1e13018114ba12d259eaf47c5e45ffb2fa1cc2
Scott Rowekamp <rowekamp@iastate.edu>
2016-09-21 16:23:27 -0500

    Starting new app to test GPS api

---
+++ iOS/Whats My Position - GPS Testing/Whats My Position - GPS Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats My Position - GPS Testing
+//
+//  Created by Scott Rowekamp on 9/21/16.
+//  Copyright © 2016 dev-scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
