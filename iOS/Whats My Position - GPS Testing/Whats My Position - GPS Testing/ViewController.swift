6290176a8bd09a79ca5df4bce8a04678e36e677e
Scott Rowekamp <rowekamp@iastate.edu>
2016-09-23 00:01:19 -0500

    Cleaned up GPS app

--- iOS/Whats My Position - GPS Testing/Whats My Position - GPS Testing/ViewController.swift
+++ iOS/Whats My Position - GPS Testing/Whats My Position - GPS Testing/ViewController.swift
@@ -10,54 +10,60 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UITableViewDelegate {
 
+    @IBOutlet weak var addressTable: UITableView!
     @IBOutlet weak var mapData: MKMapView!
-
-    @IBOutlet weak var locationLabel: UILabel!
     
-    @IBAction func getLocation(_ sender: AnyObject) {
-        print(mapData.userLocation.location)
-        locationLabel.text = "Your latitude is: \((locMan.location?.coordinate.latitude)!)"
-    }
-    
+    let geoCoder = CLGeocoder();
     let locMan = CLLocationManager();
     
+    var location_data = [String: String?]()
+    
     override func viewDidLoad() {
-        
-        
-        
         super.viewDidLoad()
         
+        // Setup Location Manager
         self.locMan.delegate = self
         self.locMan.desiredAccuracy = kCLLocationAccuracyBest
         self.locMan.requestWhenInUseAuthorization()
         self.locMan.startUpdatingLocation()
         
-        
+        // Prepare Map
         mapData.delegate = self
         mapData.userTrackingMode = MKUserTrackingMode(rawValue: 1)!
         mapData.showsUserLocation = true
-        locationLabel.text = "Location: Unknown"
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // Grab Location and Update Table
+        
     }
     
+    // MARK: - Table View Delegate Functions
     
-    // MARK: - Location Delegate Methods
+    override func table
+    
+    // MARK: - Location Functions
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last
         let center = CLLocationCoordinate2DMake((location?.coordinate.latitude)!, (location?.coordinate.longitude)!)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpanMake(1, 1))
-        mapData.setRegion(region, animated: true)
+        mapData.setRegion(region, animated: false)
         locMan.stopUpdatingLocation()
         
     }
 
-
+    func getAddress(){
+        geoCoder.reverseGeocodeLocation(locMan.location!, completionHandler: {(placemarks, error) -> Void in
+            if error == nil {
+                if (placemarks?.count)! > 0 {
+                    let pm = placemarks![0]
+                    // Store Data Here
+                    self.location_data = pm.addressDictionary as! [String : String?]
+                }
+            }
+            return
+        })
+    }
 }
 
