9c6e381482a58085461a128ce4b6e59bdc64a38f
Nathan Sorlien <nsorlien@iastate.edu>
2016-09-24 18:04:20 -0500

    Added base code to switch between views

--- Android/Assassins/app/src/main/java/la_05/com/assassins/CreateAccountActivity.java
+++ Android/Assassins/app/src/main/java/la_05/com/assassins/CreateAccountActivity.java
@@ -1,7 +1,9 @@
 package la_05.com.assassins;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+    }
+
+    /** Called when user clicks the Create Account button */
+    public void createAccount(View view) {
+        // Interact with server to create account and open the Home Page
+    }
+
+    /** Called when user clicks the log in text */
+    public void switchToLogIn(View view) {
+        // Switch to the login activity
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
--- Android/Assassins/app/src/main/java/la_05/com/assassins/LoginActivity.java
+++ Android/Assassins/app/src/main/java/la_05/com/assassins/LoginActivity.java
@@ -1,7 +1,9 @@
 package la_05.com.assassins;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    /** Called when user clicks the Login button */
+    public void logIn(View view) {
+        // Interact with server to log in and open the Home Page
+    }
+
+    /** Called when user clicks the create account text */
+    public void switchToCreateAccount(View view) {
+        // Switch to the create account activity
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
