a1217e0d5fd1ad4359b052a5e504d9887da30b02
Scott Rowekamp <rowekamp@iastate.edu>
2016-09-24 16:53:46 -0500

    Created test app to play sounds

---
+++ iOS/Kill Testing/Kill Testing/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Kill Testing
+//
+//  Created by Scott Rowekamp on 9/24/16.
+//  Copyright © 2016 dev-scott. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    
+    var player = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let path = Bundle.main.path(forResource: "snipe", ofType: ".mp3")
+        
+        let url = URL(fileURLWithPath: path!)
+        
+        do {
+            let sound = try AVAudioPlayer(contentsOf: url)
+            player = sound
+            player.prepareToPlay()
+        } catch {
+            // couldn't load file :(
+        }
+    
+    
+    }
+
+    @IBAction func button_kill(_ sender: AnyObject) {
+        player.stop()
+        player.play()
+    }
+}
+
