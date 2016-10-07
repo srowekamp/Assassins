001527a7db6f9a6b3f347294d70e3099dc1a6586
Nathan Sorlien <nsorlien@iastate.edu>
2016-10-07 14:57:33 -0500

    Finished basic login functionality

--- Server/Assassins/src/assassins/CreateAccount.java
+++ Server/Assassins/src/assassins/CreateAccount.java
@@ -6,9 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
-import java.util.Enumeration;
 import org.json.simple.JSONObject;
-
 import assassins.DBConnectionHandler;
 
 public class CreateAccount extends HttpServlet {
--- Server/Assassins/src/assassins/Login.java
+++ Server/Assassins/src/assassins/Login.java
@@ -6,9 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
-import java.util.Enumeration;
 import org.json.simple.JSONObject;
-
+import assassins.CreateAccount;
 import assassins.DBConnectionHandler;
 
 public class Login extends HttpServlet {
@@ -18,8 +17,8 @@ public class Login extends HttpServlet {
 	 */
 	private static final long serialVersionUID = -6693642062446368134L;
 	
-	public static final String KEY_USERNAME = "username";
-	public static final String KEY_PASSWORD = "password";
+	public static final String RESULT_LOGIN_SUCCESS = "success";
+	public static final String RESULT_LOGIN_FAIL = "fail";
 
     /** 
      * Handles the HTTP <code>GET</code> method.
@@ -31,41 +30,38 @@ public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        JSONObject jsonResponse = new JSONObject();
-
-        Enumeration paramNames = request.getParameterNames();
-        String params[] = new String[2];
-        int i = 0;
-        while (paramNames.hasMoreElements()) {
-            String paramName = (String) paramNames.nextElement();
-            String[] paramValues = request.getParameterValues(paramName);
-            params[i] = paramValues[0];
-            i++;
- 
+    	JSONObject jsonResponse = new JSONObject();
+        boolean accountExists = true;
+        String username = request.getParameter(CreateAccount.KEY_USERNAME);
+        String password = request.getParameter(CreateAccount.KEY_PASSWORD);
+        
+        if (!CreateAccount.isValidUsername(username)) jsonResponse.put(CreateAccount.KEY_RESULT, CreateAccount.RESULT_USERNAME_INVALID); // Check username and password for validity
+        else if (!CreateAccount.isValidPassword(password)) jsonResponse.put(CreateAccount.KEY_RESULT, CreateAccount.RESULT_PASSWORD_INVALID);
+        else {
+        	// Check if the username and password provided exist in database
+	        String sql = "SELECT username, password FROM db309la05.users where username=? and password=?";
+	        Connection con = DBConnectionHandler.getConnection();
+	        try {
+	            PreparedStatement ps = con.prepareStatement(sql);
+	            ps.setString(1, username);
+	            ps.setString(2, password);
+	            ResultSet rs = ps.executeQuery();
+	            if (rs.next()) {
+	                jsonResponse.put(CreateAccount.KEY_RESULT, RESULT_LOGIN_SUCCESS);
+	            } else {
+	            	jsonResponse.put(CreateAccount.KEY_RESULT, RESULT_LOGIN_FAIL);
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            jsonResponse.put(CreateAccount.KEY_RESULT, CreateAccount.RESULT_OTHER_ERROR);
+	        }
         }
-        String sql = "SELECT username, password FROM db309la05.users where username=? and password=?";
-        Connection con = DBConnectionHandler.getConnection();
-        String test = null;
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setString(1, params[0]);
-            ps.setString(2, params[1]);
-            ResultSet rs = ps.executeQuery();
-            test = ps.toString();
-            if (rs.next()) {
-                jsonResponse.put("info", "success");
-            } else {
-                jsonResponse.put("info", "fail");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //Write the JSON object to the response
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(jsonResponse.toString());
     }
     
- 
     /** 
      * Handles the HTTP <code>POST</code> method.
      * @param request servlet request
