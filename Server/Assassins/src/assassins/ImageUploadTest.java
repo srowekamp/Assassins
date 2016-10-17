1d2642e3269abe49a972b8d972905293be130cd1
Nathan Sorlien <nsorlien@iastate.edu>
2016-10-17 13:50:19 -0500

    Uploading an image seems to be working

--- Server/Assassins/src/assassins/ImageUploadTest.java
+++ Server/Assassins/src/assassins/ImageUploadTest.java
@@ -1,6 +1,11 @@
 package assassins;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +16,9 @@ import assassins.UserAccount;
 import assassins.DBConnectionHandler;
 
 public class ImageUploadTest extends HttpServlet {
+	
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_NAME = "name";
 
     /** 
      * Handles the HTTP <code>GET</code> method.
@@ -22,7 +30,30 @@ public class ImageUploadTest extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+    	String name = request.getParameter(KEY_NAME);
+    	String b64image = request.getParameter(KEY_IMAGE);
+    	String result = "fail";
+    	if (name != null && b64image != null && name.length() > 4 && b64image.length() > 4){
+    		saveB64Image(name, b64image);
+    		result = "success";
+    	}
+    		
     	
+    	response.getWriter().write(result);
+    }
+    
+    private boolean saveB64Image(String filename, String b64Image) {
+    	String filepath = "/var/lib/tomcat/webapps/imageTest/";
+    	byte[] imageBytes = javax.xml.bind.DatatypeConverter.parseBase64Binary(b64Image);
+    	try {
+			BufferedImage img = ImageIO.read(new ByteArrayInputStream(imageBytes));
+			File outputImage = new File(filepath + filename);
+	    	ImageIO.write(img, "jpg", outputImage);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+    	return true;
     }
     
     /** 
