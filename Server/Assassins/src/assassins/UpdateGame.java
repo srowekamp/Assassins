d5afe060039a014b2baa8e51bb3ac37b897b36ea
Nathan Sorlien <nsorlien@iastate.edu>
2016-10-27 19:00:23 -0500

    Starting work on the UpdateGame servlet

---
+++ Server/Assassins/src/assassins/UpdateGame.java
@@ -0,0 +1,48 @@
+package assassins;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONObject;
+import assassins.Game;
+
+public class UpdateGame extends HttpServlet {
+
+	public static final String KEY_RESULT = "result";
+	public static final String KEY_PARAMETER_MISSING = "parameter_error";
+
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        JSONObject jsonResponse	= new JSONObject();
+        String result = null;
+        
+        jsonResponse.put(KEY_RESULT, result);
+        //Write the JSON object to the response
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(jsonResponse.toString());
+    }
+ 
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
