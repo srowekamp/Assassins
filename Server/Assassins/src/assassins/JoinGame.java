7507141cb5d4b984e76d0c27e51a1444f1ca53ac
tjfenton <tjfenton@iastate.edu>
2016-11-05 17:05:54 -0500

    Edited join game servlet and DB

--- Server/Assassins/src/assassins/JoinGame.java
+++ Server/Assassins/src/assassins/JoinGame.java
@@ -14,6 +14,7 @@ public class JoinGame extends HttpServlet{
 	private static final long serialVersionUID = 3390648842666208917L;
 	
 	public static final String KEY_RESULT = "result";
+	public static final String RESULT_PARAMETER_MISSING = "parameter_error";
 	public static final String RESULT_JOIN_GAME_SUCCESS = "joined game";
 	public static final String RESULT_JOIN_GAME_FAILURE = "unable to join game";
 	
@@ -29,10 +30,28 @@ public class JoinGame extends HttpServlet{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException{
 		JSONObject jsonResponse = new JSONObject();
-		String gameID, password, playerList, playersAlive, endTime;
-		int id;
-		float xPosition, yPosition;
-		
+		Game tempGame = null;
+		boolean missingParameter = false;
+		String result = null;
+		try{
+			tempGame = new Game(request);
+		}
+		catch(Exception e){
+			missingParameter = true;
+			result = RESULT_PARAMETER_MISSING;
+		}
+		Game game = null;
+		if(!missingParameter){
+			String validity = tempGame.checkValidity();
+			if(validity.equals(Game.VALID)){
+				if(DB.doesGameExist(tempGame.getGameID())){
+					result = game.RESULT_GAME_EXISTS;
+				}
+				else{
+					
+				}
+			}
+		}
 		
 	}
 	
@@ -46,7 +65,7 @@ public class JoinGame extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     		throws ServletException, IOException{
-    	
+    	doGet(request, response);
     }
 
 }
