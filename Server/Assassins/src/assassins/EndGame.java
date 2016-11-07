af188067e257e15d4b07075fd7f0a14261fc2ca0
tjfenton <tjfenton@iastate.edu>
2016-11-07 10:23:15 -0600

    work on endgame servlet

--- Server/Assassins/src/assassins/EndGame.java
+++ Server/Assassins/src/assassins/EndGame.java
@@ -1,7 +1,46 @@
 package assassins;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class EndGame extends HttpServlet{
+	
+	/**
+	 * Auto-generated number
+	 */
+	private static final long serialVersionUID = 3390648842666208917L;
+	
+	public static final String KEY_RESULT = "result";
+	public static final String KEY_SUCCESS = "Game over";
+	public static final String KEY_FAILURE = "game still going";
 
+	/**
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{
+		
+	}
+	
+	/** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    		throws ServletException, IOException{
+    	doGet(request, response);
+    }
 }
